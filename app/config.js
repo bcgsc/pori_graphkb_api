ce16cf6f0cd64d28389011545a9dfac0f7aadaea
Caralyn Reisle <creisle@bcgsc.ca>
2019-08-13 16:08:53 -0700

    KBDEV-596: Point to new keycloak

--- app/config.js
+++ app/config.js
@@ -10,8 +10,9 @@ module.exports = {
         GKB_DB_USER: 'admin',
         GKB_KEYCLOAK_CLIENT_ID: 'GraphKB',
         GKB_KEYCLOAK_ROLE: 'GraphKB',
-        GKB_KEYCLOAK_URI: 'http://ga4ghdev01.bcgsc.ca:8080/auth/realms/TestKB/protocol/openid-connect/token',
-        GKB_KEYCLOAK_KEY_FILE: 'keycloak-dev.id_rsa.pub',
+        GKB_KEYCLOAK_URI: 'http://keycloakdev01.bcgsc.ca/auth/realms/GSC_posix_syncd/protocol/openid-connect/token',
+        GKB_KEYCLOAK_KEY_FILE: 'keycloak-dev.key',
+        GKB_KEYCLOAK_CLIENT_SECRET: 'c0f5118c-5cde-4537-8558-f33ff46667ae',
         GKB_KEY_FILE: 'id_rsa',
         GKB_LOG_DIR: 'logs',
         GKB_LOG_LEVEL: 'debug',
@@ -22,7 +23,7 @@ module.exports = {
     development: {
         GKB_DB_CREATE: false,
         GKB_LOG_MAX_FILES: 7,
-        GKB_DB_NAME: 'test-statement-linkset'
+        GKB_DB_NAME: 'next-production'
     },
     local: {},
     production: {
@@ -30,8 +31,8 @@ module.exports = {
         GKB_DB_HOST: 'orientdbdev.bcgsc.ca',
         GKB_DB_NAME: 'development',
         GKB_DB_PORT: 2427,
-        GKB_KEYCLOAK_KEY_FILE: 'keycloak.id_rsa.pub',
-        GKB_KEYCLOAK_URI: 'https://sso.bcgsc.ca/auth/realms/GSC/protocol/openid-connect/token',
+        GKB_KEYCLOAK_KEY_FILE: 'keycloak.key',
+        GKB_KEYCLOAK_URI: 'https://keycloak.bcgsc.ca/auth/realms/GSC/protocol/openid-connect/token',
         GKB_LOG_LEVEL: 'info',
         GKB_LOG_MAX_FILES: 28
     },
