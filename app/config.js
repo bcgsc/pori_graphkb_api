e347d884120a8a87566298d77b687997dd6e91f8
Caralyn Reisle <creisle@bcgsc.ca>
2019-09-10 10:00:23 -0700

    Add keycloak public keys to repo

--- app/config.js
+++ app/config.js
@@ -12,8 +12,7 @@ module.exports = {
         GKB_KEYCLOAK_CLIENT_ID: 'GraphKB',
         GKB_KEYCLOAK_ROLE: 'GraphKB',
         GKB_KEYCLOAK_URI: 'http://keycloakdev01.bcgsc.ca/auth/realms/GSC_posix_syncd/protocol/openid-connect/token',
-        GKB_KEYCLOAK_KEY_FILE: 'keycloak-dev.key',
-        GKB_KEYCLOAK_CLIENT_SECRET: 'c0f5118c-5cde-4537-8558-f33ff46667ae',
+        GKB_KEYCLOAK_KEY_FILE: 'config/keys/keycloak-dev.key',
         GKB_KEY_FILE: 'id_rsa',
         GKB_LOG_DIR: 'logs',
         GKB_LOG_LEVEL: 'debug',
@@ -26,15 +25,17 @@ module.exports = {
         GKB_DB_CREATE: false,
         GKB_LOG_MAX_FILES: 7,
         GKB_DB_NAME: 'next-production',
-        GKB_CORS_ORIGIN: 'https://graphkbdev.bcgsc.ca'
+        GKB_CORS_ORIGIN: 'https://graphkbdev.bcgsc.ca http://localhost:3000'
     },
-    local: {},
+    local: {
+        GKB_CORS_ORIGIN: 'http://localhost:3000'
+    },
     production: {
         GKB_DB_CREATE: false,
         GKB_DB_HOST: 'orientdbdev.bcgsc.ca',
         GKB_DB_NAME: 'development',
         GKB_DB_PORT: 2427,
-        GKB_KEYCLOAK_KEY_FILE: 'keycloak.key',
+        GKB_KEYCLOAK_KEY_FILE: 'config/keys/keycloak.key',
         GKB_KEYCLOAK_URI: 'https://keycloak.bcgsc.ca/auth/realms/GSC/protocol/openid-connect/token',
         GKB_LOG_LEVEL: 'info',
         GKB_LOG_MAX_FILES: 28,
