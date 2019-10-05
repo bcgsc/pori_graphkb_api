0e8e27728ad404f2ab66fe4af8d65c686add9480
Caralyn Reisle <creisle@bcgsc.ca>
2019-10-04 17:20:07 -0700

    Remove old custom query routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,98 +9,11 @@ const openapi = require('./openapi');
 const resource = require('./resource');
 const {logger} = require('./../repo/logging');
 const {
-    MIN_WORD_SIZE, checkStandardOptions
+    checkStandardOptions
 } = require('./query');
-const {selectByKeyword, selectFromList, selectCounts} = require('../repo/commands');
+const {selectCounts} = require('../repo/commands');
 const {addErrorRoute} = require('./error');
-
-/**
- * @param {AppServer} app the GraphKB app server
- */
-const addKeywordSearchRoute = (app) => {
-    logger.log('verbose', 'NEW ROUTE [GET] /statements/search');
-
-    app.router.get('/statements/search',
-        async (req, res, next) => {
-            const {
-                keyword
-            } = req.query;
-
-            const options = {user: req.user};
-            try {
-                Object.assign(options, checkStandardOptions(req.query));
-            } catch (err) {
-                return next(err);
-            }
-            if (keyword === undefined) {
-                return res.status(HTTP_STATUS.BAD_REQUEST).json({
-                    message: 'keyword query parameter is required'
-                });
-            }
-            const wordList = keyword.split(/\s+/);
-
-            if (wordList.some(word => word.length < MIN_WORD_SIZE)) {
-                return res.status(HTTP_STATUS.BAD_REQUEST).json(new AttributeError(
-                    `Word "${keyword}" is too short to query with ~ operator. Must be at least ${
-                        MIN_WORD_SIZE
-                    } letters after splitting on whitespace characters`
-                ));
-            }
-            let session;
-            try {
-                session = await app.pool.acquire();
-            } catch (err) {
-                return next(err);
-            }
-            try {
-                const result = await selectByKeyword(session, wordList, options);
-                session.close();
-                return res.json(jc.decycle({result}));
-            } catch (err) {
-                session.close();
-                return next(err);
-            }
-        });
-};
-
-/**
- * @param {AppServer} app the GraphKB app server
- */
-const addGetRecordsByList = (app) => {
-    app.router.get('/records',
-        async (req, res, next) => {
-            let options;
-            try {
-                options = {...checkStandardOptions(req.query), user: req.user};
-            } catch (err) {
-                return next(err);
-            }
-
-            const {
-                rid = '', activeOnly, neighbors, user, ...rest
-            } = options;
-            if (Object.keys(rest).length) {
-                return next(new AttributeError({
-                    message: `Invalid query parameter(s) (${Object.keys(rest).join(', ')})`,
-                    invalidParams: rest
-                }));
-            }
-            let session;
-            try {
-                session = await app.pool.acquire();
-            } catch (err) {
-                return next(err);
-            }
-            try {
-                const result = await selectFromList(session, rid.split(',').map(r => r.trim()), options);
-                session.close();
-                return res.json(jc.decycle({result}));
-            } catch (err) {
-                session.close();
-                return next(err);
-            }
-        });
-};
+const {addQueryRoute} = require('./query');
 
 
 const addStatsRoute = (app) => {
@@ -151,5 +64,5 @@ const addParserRoute = (app) => {
 
 
 module.exports = {
-    openapi, resource, addKeywordSearchRoute, addGetRecordsByList, addStatsRoute, addParserRoute, addErrorRoute
+    openapi, resource, addStatsRoute, addParserRoute, addErrorRoute, addQueryRoute
 };
