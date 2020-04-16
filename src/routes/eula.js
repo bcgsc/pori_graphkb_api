3fb66a0be71e9ef95c3e3ffc9d582bd66f6225c6
Caralyn Reisle <creisle@bcgsc.ca>
2020-04-15 18:35:45 -0700

    Add the eula route module

---
+++ src/routes/eula.js
@@ -0,0 +1,117 @@
+
+const { util: { timeStampNow }, schema: { schema: SCHEMA_DEFN } } = require('@bcgsc/knowledgebase-schema');
+const { logger } = require('./../repo/logging');
+
+const { RecordNotFoundError, PermissionError } = require('./../repo/error');
+
+
+const getCurrentLicense = async db => db.query(
+    `SELECT * FROM ${SCHEMA_DEFN.LicenseAgreement.name} ORDER BY enactedAt DESC LIMIT 1`,
+).one();
+
+/**
+ * Route to query the db
+ *
+ * @param {AppServer} app the GraphKB app server
+ */
+const addEulaRoutes = (app) => {
+    app.router.get('/license',
+        async (req, res, next) => {
+            let session;
+
+            try {
+                session = await app.pool.acquire();
+            } catch (err) {
+                return next(err);
+            }
+
+            try {
+                const result = await getCurrentLicense(session);
+                return res.json(result);
+            } catch (err) {
+                session.close();
+                logger.log('debug', err);
+                return next(err);
+            }
+        });
+
+    app.router.post('/license',
+        async (req, res, next) => {
+            const { body: content } = req;
+            let session;
+
+            try {
+                session = await app.pool.acquire();
+            } catch (err) {
+                return next(err);
+            }
+
+            try {
+                const result = await session.insert().into(SCHEMA_DEFN.LicenseAgreement.name).set({
+                    content,
+                    enactedAt: timeStampNow(),
+                }).one();
+                session.close();
+                return res.json(result);
+            } catch (err) {
+                session.close();
+                logger.log('debug', err);
+                return next(err);
+            }
+        });
+
+    app.router.post('/license/sign',
+        async (req, res, next) => {
+            const { user } = req;
+            let session;
+
+            try {
+                session = await app.pool.acquire();
+            } catch (err) {
+                return next(err);
+            }
+
+            try {
+                const record = await session.record.get(user['@rid']);
+
+                if (!user) {
+                    throw new RecordNotFoundError(`No user with the ID ${user['@rid']}`);
+                }
+                const result = await session.record.update({ ...record, signedLicenseAt: timeStampNow() });
+                session.close();
+                return res.json(result);
+            } catch (err) {
+                session.close();
+                logger.log('debug', err);
+                return next(err);
+            }
+        });
+
+    // add the route to enforce unsigned users cannot access
+    app.router.use(async (req, res, next) => {
+        const { user } = req;
+        let session;
+
+        try {
+            session = await app.pool.acquire();
+        } catch (err) {
+            return next(err);
+        }
+
+        try {
+            const license = await getCurrentLicense(session);
+
+            if (!user.signedLicenseAt || license.enactedAt > user.signedLicenseAt) {
+                throw new PermissionError('User must sign the license agreement before they can access data. See https://graphkb.bcgsc.ca/about/terms');
+            }
+            session.close();
+            return next();
+        } catch (err) {
+            session.close();
+            logger.log('debug', err);
+            return next(err);
+        }
+    });
+};
+
+module.exports = { addEulaRoutes };
