ae2b66f34f994a5d97f88bab920c8193e28e18f4
mathieulemieux <mathieu@mathieulemieux.com>
2025-07-16 13:08:10 -0700

    Add and fix tests in test/repo/subgraphs/util.test.js

--- test/repo/subgraphs/util.test.js
+++ test/repo/subgraphs/util.test.js
@@ -28,30 +28,63 @@ const {
     RECORDS_MAP,
 } = require('./data');
 
-describe('getAdjacency', () => {
-    test('undirected adjacency list', () => {
-        const result = getAdjacency(GRAPH);
-        expect(result).toEqual(ADJACENCY); // default to undirected
+describe('buildTraverseExpr', () => {
+    const edges = ['A', 'B'];
+    const treeEdges = ['C', 'D'];
+
+    test('traverse expression for traversal query; no direction', () => {
+        const result = buildTraverseExpr({ edges, prefix: 't_', treeEdges });
+        const expected = `
+            both('A'),bothE('A'),
+            both('B'),bothE('B')
+        `;
+        expect(result).toBe(oneliner(expected, false));
     });
 
-    test('directed adjacency list', () => {
-        const result = getAdjacency(GRAPH, { directed: true });
-        expect(result).toEqual(ADJACENCY_DIRECTED);
+    test('traverse expression for traversal query; ascending', () => {
+        const result = buildTraverseExpr({
+            direction: 'ascending',
+            edges,
+            treeEdges,
+        });
+        const expected = `
+            both('A'),bothE('A'),
+            both('B'),bothE('B'),
+            out('C'),outE('C'),
+            out('D'),outE('D')
+        `;
+        expect(result).toBe(oneliner(expected, false));
     });
-});
 
-describe('getComponents', () => {
-    test('disconnected components', () => {
-        const result = getComponents(ADJACENCY);
+    test('traverse expression for traversal query; descending', () => {
+        const result = buildTraverseExpr({
+            direction: 'descending',
+            edges,
+            treeEdges,
+        });
+        const expected = `
+            both('A'),bothE('A'),
+            both('B'),bothE('B'),
+            in('C'),inE('C'),
+            in('D'),inE('D')
+        `;
+        expect(result).toBe(oneliner(expected, false));
+    });
 
-        // number of components
-        expect(result.length).toEqual(COMPONENTS.length);
-
-        // each component array
-        for (let i = 0; i < result.length; i++) {
-            expect(result[i]).toEqual(COMPONENTS[i]);
-            // expect(result[i].sort()).toEqual(COMPONENTS[i].sort());
-        }
+    test('traverse expression for traversal query; without the edges', () => {
+        const result = buildTraverseExpr({
+            direction: 'descending',
+            edges,
+            treeEdges,
+            withEdges: false,
+        });
+        const expected = `
+            both('A'),
+            both('B'),
+            in('C'),
+            in('D')
+        `;
+        expect(result).toBe(oneliner(expected, false));
     });
 });
 
@@ -87,6 +120,54 @@ describe('formatToFlowchart', () => {
             nodes,
         });
         expect(result).toEqual(flowchart.join('\n'));
+    });
+});
+
+describe('getAdjacency', () => {
+    test('undirected adjacency list', () => {
+        const result = getAdjacency(GRAPH);
+        expect(result).toEqual(ADJACENCY); // default to undirected
+    });
+
+    test('directed adjacency list', () => {
+        const result = getAdjacency(GRAPH, { directed: true });
+        expect(result).toEqual(ADJACENCY_DIRECTED);
+    });
+});
+
+describe('getComponents', () => {
+    test('disconnected components', () => {
+        const result = getComponents(ADJACENCY);
+
+        // number of components
+        expect(result.length).toEqual(COMPONENTS.length);
+
+        // each component array
+        for (let i = 0; i < result.length; i++) {
+            expect(result[i]).toEqual(COMPONENTS[i]);
+        }
+    });
+});
+
+describe('getGraph', () => {
+    test('format records into a graph object', () => {
+        const graph = getGraph(RECORDS_MAP, { propsPerClass: PROPS_PER_CLASS });
+        expect(graph.nodes).toEqual(NODES);
+        expect(graph.edges).toEqual(EDGES);
+        expect(graph).toEqual(GRAPH);
+    });
+
+    test('removes unwanted properties', () => {
+        // removing 'name' from accepted properties on Disease
+        const propsPerClass = new Map([...PROPS_PER_CLASS]);
+        propsPerClass.set('Disease', propsPerClass.get('Disease').filter((x) => x !== 'name'));
+
+        const graph = getGraph(RECORDS_MAP, { propsPerClass });
+        const r1 = [...graph.nodes.values()][0]; // 1st node as any node
+        const r1Props = Object.keys(r1);
+
+        expect(r1Props.includes('name')).toBe(false);
+        expect(r1Props.includes('source.sort')).toBe(true);
     });
 });
 
@@ -136,76 +217,6 @@ describe('getInheritingClasses', () => {
     });
 });
 
-describe('buildTraverseExpr', () => {
-    const edges = ['A', 'B'];
-    const treeEdges = ['C', 'D'];
-    const expectedParams = {
-        t_edge0: 'A',
-        t_edge1: 'B',
-        t_treeEdge0: 'C',
-        t_treeEdge1: 'D',
-    };
-
-    test('traverse expression for traversal query; no direction', () => {
-        const result = buildTraverseExpr({ edges, prefix: 't_', treeEdges });
-        const expected = `
-            both(:t_edge0),bothE(:t_edge0),
-            both(:t_edge1),bothE(:t_edge1)
-        `;
-        expect(result.expr).toBe(oneliner(expected, false));
-        expect(result.params).toEqual(_.pick(expectedParams, ['t_edge0', 't_edge1']));
-    });
-
-    test('traverse expression for traversal query; ascending', () => {
-        const result = buildTraverseExpr({
-            direction: 'ascending',
-            edges,
-            treeEdges,
-        });
-        const expected = `
-            both(:t_edge0),bothE(:t_edge0),
-            both(:t_edge1),bothE(:t_edge1),
-            out(:t_treeEdge0),outE(:t_treeEdge0),
-            out(:t_treeEdge1),outE(:t_treeEdge1)
-        `;
-        expect(result.expr).toBe(oneliner(expected, false));
-        expect(result.params).toEqual(expectedParams);
-    });
-
-    test('traverse expression for traversal query; descending', () => {
-        const result = buildTraverseExpr({
-            direction: 'descending',
-            edges,
-            treeEdges,
-        });
-        const expected = `
-            both(:t_edge0),bothE(:t_edge0),
-            both(:t_edge1),bothE(:t_edge1),
-            in(:t_treeEdge0),inE(:t_treeEdge0),
-            in(:t_treeEdge1),inE(:t_treeEdge1)
-        `;
-        expect(result.expr).toBe(oneliner(expected, false));
-        expect(result.params).toEqual(expectedParams);
-    });
-
-    test('traverse expression for traversal query; without the edges', () => {
-        const result = buildTraverseExpr({
-            direction: 'descending',
-            edges,
-            treeEdges,
-            withEdges: false,
-        });
-        const expected = `
-            both(:t_edge0),
-            both(:t_edge1),
-            in(:t_treeEdge0),
-            in(:t_treeEdge1)
-        `;
-        expect(result.expr).toBe(oneliner(expected, false));
-        expect(result.params).toEqual(expectedParams);
-    });
-});
-
 describe('getPropsPerClass', () => {
     test('return expected props on each class', () => {
         const result = getPropsPerClass(
@@ -220,23 +231,20 @@ describe('getPropsPerClass', () => {
     });
 });
 
-describe('getGraph', () => {
-    test('format records into a graph object', () => {
-        const graph = getGraph(RECORDS_MAP, { propsPerClass: PROPS_PER_CLASS });
-        expect(graph.nodes).toEqual(NODES);
-        expect(graph.edges).toEqual(EDGES);
-        expect(graph).toEqual(GRAPH);
+describe('oneliner', () => {
+    test('format multiline string into a one line string', () => {
+        const result = oneliner(`
+            A B
+            C
+            D`);
+        expect(result).toEqual('A B C D');
     });
 
-    test('removes unwanted properties', () => {
-        const propsPerClass = PROPS_PER_CLASS;
-        propsPerClass.set('Disease', propsPerClass.get('Disease').filter((x) => x !== 'name'));
-
-        const graph = getGraph(RECORDS_MAP, { propsPerClass });
-
-        const r1 = [...graph.nodes.values()][0];
-        const r1Props = Object.keys(r1);
-        expect(r1Props.includes('name')).toBe(false);
-        expect(r1Props.includes('source.sort')).toBe(true);
+    test('format multiline string into a one line string, without space replacing new lines', () => {
+        const result = oneliner(`
+            A B
+            C
+            D`, false);
+        expect(result).toEqual('A BCD');
     });
 });
