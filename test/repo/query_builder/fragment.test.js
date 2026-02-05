04b67ac604de5c0ada907e5e5a4b5067131b7688
mathieulemieux <mathieu@mathieulemieux.com>
2026-02-04 17:40:30 -0800

    Add tests for Comparison.validate()

---
+++ test/repo/query_builder/fragment.test.js
@@ -0,0 +1,26 @@
+const { ValidationError } = require('@bcgsc-pori/graphkb-schema');
+
+const { Comparison } = require('../../../src/repo/query_builder/fragment');
+
+describe('Comparison', () => {
+    const newComparison = new Comparison({
+        name: 'Ontology',
+        operator: 'CONTAINSTEXT',
+        prop: 'name',
+        value: 'abcdef',
+    });
+
+    test('not to throw error on valid CONTAINSTEXT value', () => {
+        expect(() => newComparison.validate()).not.toThrow(ValidationError);
+    });
+
+    test('throw error on CONTAINSTEXT value containing whitespaces', () => {
+        newComparison.value = 'abc def';
+        expect(() => newComparison.validate()).toThrow(ValidationError);
+    });
+
+    test('throw error on CONTAINSTEXT value containing seperator chars', () => {
+        newComparison.value = 'abc:def';
+        expect(() => newComparison.validate()).toThrow(ValidationError);
+    });
+});
