84ebc8f7acbb4afb3d1f72fd02cd0e3370aa9fc7
mathieulemieux <mathieu@mathieulemieux.com>
2026-05-21 10:43:23 -0700

    Add back metadata logging in rebuildIndexes()

--- test/db_integration/util.js
+++ test/db_integration/util.js
@@ -74,6 +74,11 @@ const rebuildIndexes = async (session) => {
             METADATA ${JSON.stringify(metadata)}
         `).all();
     }
+    // Logging indexes metadata
+    const metadata = await session.query(`
+        SELECT * FROM (SELECT expand(indexes) FROM metadata:indexmanager) WHERE name LIKE '%_fulltext'
+    `).all();
+    throw new Error(JSON.stringify(metadata));
 };
 
 /**
